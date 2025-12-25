85400352269ee7b3b5d9bbb57bfeca4bbaba905d
imimoun <ilan.mimoun.work@gmail.com>
2025-12-25 15:01:17 +0200

    connect to n8n

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,8 +10,11 @@ export class App {
   title = 'Equipment Installation Check';
 
   callN8n() {
-    console.log('Calling n8n workflow...');
-    // Add your webhook logic here:
-    // fetch('YOUR_N8N_WEBHOOK_URL', { method: 'POST' });
+    fetch(
+      'https://hillelsu.app.n8n.cloud/webhook-test/8d3f8c02-f6bc-496b-b4e9-fd6ef7d5762a',
+      {
+        method: 'POST'
+      }
+    );
   }
 }
